8c3231891c841b72c9df4270f52dc494f2ab091a
Martin-Rojas <rojas.martin25@yahoo.com>
2025-09-10 19:08:53 -0700

    code to make an API call and display it into console

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./styles.css";
+
+const location = `pflugerville`;
+const APIUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=45P5NZGENNKHBPNP5YQZZC7NP`;
+
+async function getWeather() {
+  const response = await fetch(APIUrl);
+  const weatherData = await response.json();
+  console.log(weatherData.currentConditions);
+}
+getWeather();
