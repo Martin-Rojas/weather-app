20f02b7b1b47432bd3d37c0a75c199840ca722db
Martin-Rojas <rojas.martin25@yahoo.com>
2025-09-13 20:41:44 -0700

    code basic structure of html

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ const location = `pflugerville`;
 const APIUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=45P5NZGENNKHBPNP5YQZZC7NP`;
 
 async function getWeather() {
-  const response = await fetch(APIUrl);
+  const response = await fetch(APIUrl).catch((err) => console.log(err));
   const weatherData = await response.json();
   console.log(weatherData.currentConditions);
+  const displayWeatherData = {
+    dateTime: weatherData.currentConditions.datetime,
+    tempMax: weatherData.currentConditions.temp,
+    humidity: weatherData.currentConditions.humidity
+  };
+  return displayWeatherData;
 }
-getWeather();
+const data = getWeather()
+console.log(data);
