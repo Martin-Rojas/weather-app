5310fc9f04b9b1ab69be3680be9b47f5535354ec
Martin-Rojas <rojas.martin25@yahoo.com>
2025-09-16 15:31:46 -0700

    code update to displat address and icon img

--- src/index.js
+++ src/index.js
@@ -3,16 +3,31 @@ import "./styles.css";
 const location = `pflugerville`;
 const APIUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=45P5NZGENNKHBPNP5YQZZC7NP`;
 
+const addressEl = document.getElementById(`address`);
+const currentTempIcon = document.getElementById(`weather-icon`);
+
 async function getWeather() {
   const response = await fetch(APIUrl).catch((err) => console.log(err));
   const weatherData = await response.json();
-  console.log(weatherData.currentConditions);
+
   const displayWeatherData = {
     dateTime: weatherData.currentConditions.datetime,
     tempMax: weatherData.currentConditions.temp,
-    humidity: weatherData.currentConditions.humidity
+    humidity: weatherData.currentConditions.humidity,
+    currentPlace: weatherData.resolvedAddress,
+    icon: weatherData.currentConditions.icon,
   };
+
   return displayWeatherData;
 }
-const data = getWeather()
-console.log(data);
+
+async function displayData() {
+  const data = await getWeather();
+
+  // Address
+  addressEl.innerHTML = data.currentPlace;
+
+  // Icon imgs
+  currentTempIcon.setAttribute(`src`, `./images/${data.icon}.png`);
+}
+displayData();
